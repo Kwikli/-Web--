3d70c39c7713cd7ad110175db6214607df17fbe0
Владислав <95309036+Kwikli@users.noreply.github.com>
2022-09-26 17:10:33 +0300

    js

--- css/main.js
+++ css/main.js
@@ -1,2 +1,18 @@
 new WOW().init();
-console.log('sdsdsds');
+
+const cartButton = document.querySelector("#cart_button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+
+cartButton.addEventListener("click", function(event){
+    modal.classList.add("is-open");
+});
+close.addEventListener("click", function(event){
+    modal.classList.remove("is-open");
+});
+
+
+function toggleModal(){
+    modal.classList.toggle("is-open");
+}
